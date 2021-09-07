e6bb969d4a69de1e87b0e7b4ddc9eb0830b9ce20
Peter Salz <psalz.app.dev@web.de>
2021-09-07 22:02:58 +0200

    Initial Commit

---
+++ Sources/PSActivityImageViewController/PSActivityImageViewController.swift
@@ -0,0 +1,3 @@
+struct PSActivityImageViewController {
+    var text = "Hello, World!"
+}
---
+++ Tests/PSActivityImageViewControllerTests/PSActivityImageViewControllerTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import PSActivityImageViewController
+
+    final class PSActivityImageViewControllerTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(PSActivityImageViewController().text, "Hello, World!")
+        }
+    }
